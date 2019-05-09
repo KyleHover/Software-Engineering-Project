d97bf2c48820133e830cc12c8c4ded094fd7ff17
Mark Reff <csc190@localhost.localdomain>
2019-05-08 23:08:58 -0400

    Marko 2 electrico boog

--- src/EvilCraftMilestone3/GameEngineMS3.java
+++ src/EvilCraftMilestone3/GameEngineMS3.java
@@ -172,12 +172,9 @@ public class GameEngineMS3 extends EvilCraftGameEngine {
         this.viewportY = y;
     }
 
-<<<<<<< HEAD
-=======
     @Override
     public void onMouseMoved(ICanvasDevice canvas, int x, int y) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
->>>>>>> origin/NEW_MODULE_B
 }
